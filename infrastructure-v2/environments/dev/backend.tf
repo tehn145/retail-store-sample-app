64869bd0b64b7233c6bb77aed5f6492b9cf0c4a7
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-05-19 14:19:02 +0700

    Update backend.tf

--- infrastructure-v2/environments/dev/backend.tf
+++ infrastructure-v2/environments/dev/backend.tf
@@ -5,10 +5,10 @@
 terraform {
   backend "s3" {
     # Thay đổi sau khi bootstrap
-    bucket         = "voting-app-terraform-state-248195880649"  # Change this to your bucket name
+    bucket         = "retail-app-terraform-state-<>"  # Change this to your bucket name
     key            = "environments/dev/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "voting-app-terraform-lock-248195880649"
+    dynamodb_table = "retail-app-terraform-lock-<>"
     encrypt        = true
   }
 }
