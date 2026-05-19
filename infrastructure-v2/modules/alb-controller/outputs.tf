e2fb02d261fbda9b2a823305062bf206a3f7438b
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-05-19 14:03:05 +0700

    Delete infrastructure-v2 directory

--- infrastructure-v2/modules/alb-controller/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_controller_status" {
-  description = "ALB controller deployment status"
-  value       = helm_release.aws_load_balancer_controller.status
-}
--- infrastructure-v2/modules/argocd/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "argocd_namespace" {
-  description = "ArgoCD namespace"
-  value       = kubernetes_namespace.argocd.metadata[0].name
-}
-
-output "argocd_status" {
-  description = "ArgoCD deployment status"
-  value       = helm_release.argocd.status
-}
