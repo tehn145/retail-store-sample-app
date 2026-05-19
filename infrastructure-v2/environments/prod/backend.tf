640c87d8d84eabfb9ae81dfc92dc628a5357b098
Huycon2352 <156405801+Huycon2352@users.noreply.github.com>
2026-05-19 14:21:45 +0700

    Update S3 bucket and DynamoDB table names

--- infrastructure-v2/environments/prod/backend.tf
+++ infrastructure-v2/environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "voting-app-terraform-state-123456789"  # Change this to your bucket name
+    bucket         = "retail-app-terraform-state-<>"  # Change this to your bucket name
     key            = "environments/prod/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "voting-app-terraform-lock"
+    dynamodb_table = "retail-app-terraform-lock"
     encrypt        = true
   }
 }
